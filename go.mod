1d7a4a48e21c32d02d8aa0f98366f37b50c07567
Soumadip <39thdecember@gmail.com>
2026-01-26 01:33:54 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/callmeskyy111/notes-api
+
+go 1.24.4
---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	MongoURI string
+	MongoDB string
+	ServerPort string
+}
+
+func Load()(Config, error){
+	// godotenv.Load() - Reads the .env and sets them into the process environment.
+	if err:=godotenv.Load(); err!=nil{
+		return Config{},fmt.Errorf("⚠️ FAILED to load .env! -",err)
+	} 
+	
+		mongoURI,err:= extractEnv("MONGO_URI")
+		if err != nil {
+			return Config{},err
+		}
+		mongoDB,err:= extractEnv("MONGODB_NAME")
+		if err != nil {
+			return Config{},err
+		}
+		port,err:= extractEnv("PORT")
+		if err != nil {
+			return Config{},err
+		}
+
+		return Config{
+			MongoURI: mongoURI,
+			MongoDB: mongoDB,
+			ServerPort: port,
+		},nil
+
+}
+
+// Helper f(x)
+func extractEnv(key string)(string,error){
+	val:=os.Getenv(key)
+
+	if val==""{
+		return "",fmt.Errorf("⚠️ Missing required .env!")
+	}
+	return val,nil
+}
