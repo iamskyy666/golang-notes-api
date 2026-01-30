6ae78c364e0c49d809fbc440dc90c5189c9ec8cf
Soumadip <39thdecember@gmail.com>
2026-01-30 16:06:49 +0530

    Create CreateNote Handler ( ) ✅

---
+++ internal/notes/note.handler.go
@@ -0,0 +1,55 @@
+package notes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Handler struct {
+	repo *Repo
+}
+
+func NewHandler(repo *Repo) *Handler {
+	return &Handler{
+		repo: repo,
+	}
+}
+
+// CRUD
+
+func (h *Handler) CreateNote(ctx *gin.Context){
+	var req CreateNoteRequest
+
+	if err:= ctx.ShouldBindJSON(&req);err!=nil{
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error":"⚠️ Invalid JSON!",
+			"status_code":http.StatusBadRequest,
+		})
+		return
+	}
+
+	now:=time.Now().UTC()
+
+	note:=Note{
+		ID: primitive.NewObjectID(),
+		Title: req.Title,
+		Content: req.Content,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	createdNote,err:=h.repo.Create(ctx.Request.Context(),note)
+	if err!=nil{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error":"⚠️ Failed to CREATE note!",
+			"status_code":http.StatusInternalServerError,
+		})
+		return
+	}
+
+	// If everything is OK.. ✅
+	ctx.JSON(http.StatusCreated,createdNote)
+}
